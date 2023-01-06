4040d430e9791fd8547165faa99e22f341699f5d
Eliakim Zacarias <eliakim.zacarias@gmail.com>
2023-01-05 21:17:05 -0300

    Fix mistake and add Content-Type header.

--- ServeStatic.js
+++ ServeStatic.js
@@ -1,7 +1,7 @@
 const fs = require('node:fs');
 
 exports.ServeStatic = function (req, res) {
-    if ((req.method !== "GET") || (req.method !== "OPTIONS"))
+    if ((req.method !== "GET") && (req.method !== "OPTIONS"))
         return console.log('Unhandled', req.method, req.url);
 
     const filename = __dirname + "/static/" + (req.url.replace(/[^a-z.]/, '') || "index.html");
@@ -12,7 +12,17 @@ exports.ServeStatic = function (req, res) {
             res.end("Access deined or file not found");
             return;
         }
-        res.writeHead(200);
+
+        let contentType = 'text/html';
+        if (filename.endsWith('.css'))
+            contentType = 'text/css';
+        else if (filename.endsWith('.js'))
+            contentType = 'text/javascript';
+        else if (filename.endsWith('.ico'))
+            contentType = 'image/x-icon';
+        else if (filename.endsWith('.png'))
+            contentType = 'image/png';
+        res.writeHead(200, { 'Content-Type': contentType });
         res.end(data);
     });
 }
